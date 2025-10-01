f2f9f7b1f179326ed60dda705b72259ac21bd6d6
Jhon Trujillo Serrano <jhontruse@gmail.com>
2025-09-30 20:54:15 -0500

    Hasta este punto es el configuracion de JWT

--- src/main/java/com/pe/intranet/wsschool/exception/type/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/pe/intranet/wsschool/exception/type/JwtAuthenticationEntryPoint.java
@@ -15,7 +15,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
-//PASO 12 -JWT
+//PASO 12 -JWT. 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
